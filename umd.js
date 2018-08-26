6338429b0396565efcad76cfadc0128b8ad9edb1
YanagiEiichi <admin@web-tinker.com>
2018-08-27 01:09:26 +0800

    1.3.2
    - Capture scroll event
    - Change default width and make it configurable
    - Close panel after ok

--- umd.js
+++ umd.js
@@ -1,3 +1,22 @@
+/**/ void function(factory) {
+/**/   switch (true) {
+/**/   case typeof module === 'object' && module && 'exports' in module: // CommonJS
+/**/     module.exports = factory();
+/**/     if (module.exports === void 0) module.exports = {};
+/**/     break;
+/**/   case typeof define === 'function' && !!define.amd: // AMD
+/**/     define(factory);
+/**/     break;
+/**/   default: // Global
+/**/     var global = new Function('return this')();
+/**/     var name = 'DatePicker';
+/**/     /**/ try { /* Fuck IE8- */
+/**/     /**/   if (typeof execScript === 'object') execScript('var ' + name);
+/**/     /**/ } catch(error) {}
+/**/     global[name] = factory();
+/**/   }
+/**/ }(function() {
+
 var DatePicker;
 
 {
@@ -569,3 +588,9 @@ var DatePicker;
   }
 
 }
+
+
+return DatePicker;
+
+/**/ });
+
